7f468570adfc9441d7e178af4c5a760ba840fbf4
ToastedToast <hey@toasted.dev>
2024-09-17 18:33:31 +0800

    feat: add rng

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
-import { getCurrentGeneration } from "./utils/db";
+import { addGeneration, getCurrentGeneration, setCompleted } from "./utils/db";
 import { Hono } from "hono";
 
 const app = new Hono();
 
+function random(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+setInterval(() => {
+  const randomNumber = random(1, 100_000_000);
+  if (randomNumber === 69_696_969) {
+    setCompleted(true);
+  }
+
+  addGeneration({
+    time: Date.now(),
+    generation: randomNumber,
+  });
+}, 1000);
+
 app.get("/", (c) => {
-  console.log(getCurrentGeneration());
   return c.text("Hello World!");
+});
+
+app.get("/generations/current", (c) => {
+  const generation = getCurrentGeneration();
+  return c.json(generation);
 });
 
 const server = Bun.serve({
