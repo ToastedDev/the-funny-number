2a0f8a73491874400aef18f03a82102a17566dfb
ToastedToast <hey@toasted.dev>
2024-09-17 18:24:43 +0800

    feat: add hono

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello World!");
+});
+
+const server = Bun.serve({
+  fetch: app.fetch,
+  port: parseInt(process.env.PORT ?? "3000"),
+});
+console.log(`Listening on ${server.hostname}:${server.port}`);
